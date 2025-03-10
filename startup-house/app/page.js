6b227c9a72508d54917a6d29bf9ed7d7177bff46
sydneyhopp <sydneyhoppenworth@gmail.com>
2025-03-10 03:30:42 -0700

    almost done

--- startup-house/app/page.js
+++ startup-house/app/page.js
@@ -9,8 +9,8 @@ import PairStick from '@/components/PairStick';
 import door from '/public/illustrations/frontdoor.png';
 import kitchen from '/public/final/kitchen.svg';
 import livingRoom from '/public/final/livingroom.svg';
-import bedroom from '/public/illustrations/bedroom.png';
-import skyline from '/public/illustrations/skyline.png';
+import bedroom from '/public/final/bed.svg';
+import skyline from '/public/final/sunset.svg';
 import transish from '/public/illustrations/tiling.png';
 import coworkRoom from '/public/illustrations/workroom.png';
 import block from '/public/final/lineup.svg';
@@ -32,6 +32,12 @@ export default function Scrolly(){
       { text: '"This is a robot someone made that checks if you washed your dishes. Another startup home has something similar but it takes photos of you and your dirty dishes and tweets it."', start: 0.0, end: 0.2, leave: 0.5},
       { text: 'Fellow, a coffee machine-making brand, gifted SF Parc this machine. It’s put to good use every night. “When I come down here at 3 in the morning, most people are still working."', start: 0.4, end: 0.6, leave: 1.0}
     ]
+
+    const bedText = [
+      { text: '“I was working 110 hour weeks in Arcadia (Berkley Residency), so I didn’t talk to anyone, I was kind of just locked in my room all day. Now I’m working 80 hour weeks, so that’s better, and I can actually talk to people. I’m almost always working in my room or a coffee shop."', start: 0.0, end: 0.1, leave: 0.4},
+      { text: '"Oh, yeah, there’s someone living in there.” Another startup founder is living in the small room attached to his own.', start: 0.3, end: 0.5, leave: 0.7},
+      { text: '"These were here when I moved in.” Arraya thought they matched the futuristic styled interior design, highlighted with geometric shapes and bright colors.', start: 0.6, end: 0.8, leave: 1.0},
+    ];
 
     return (
         <main>
@@ -69,14 +75,14 @@ export default function Scrolly(){
             </section>
           </AnimateOnScroll>
 
-          
-          <section className="textBetween">
           <AnimateOnScroll reappear>
-            <p>
-              According to a study in 2020 by Junior Achievement USA, 66 percent of teens between the ages of 13 and 17 said they were "likely" to consider starting a business or becoming an entrepreneur. But 51 percent said they would need more information on what it would take to be successful, and 38 percent said they would need friends with similar interests. 
+          <div className="leftText">
+            <p>Step into SF Parc, a startup home established in July of 2024 by Nick Linck. After graduating from the University of Michigan, he founded The Residency in 2022. Since then, it has expanded to nine startup homes worldwide. Here, “college meets accelerator.”
             </p>
-            </AnimateOnScroll>
-          </section>
+          </div>
+          </AnimateOnScroll>
+
+          
           
 
           <AnimateOnScroll reappear>
@@ -98,7 +104,17 @@ export default function Scrolly(){
           </section>
           </AnimateOnScroll>
 
-          <section className="textBetween">
+          <AnimateOnScroll reappear>
+          <div className="rightContainer">
+          <div className="rightText">
+            <p>
+              According to a study in 2020 by Junior Achievement USA, 66 percent of teens between the ages of 13 and 17 said they were "likely" to consider starting a business or becoming an entrepreneur. But 51 percent said they would need more information on what it would take to be successful, and 38 percent said they would need friends with similar interests. 
+            </p>
+          </div>
+          </div>
+          </AnimateOnScroll>
+
+          <section className="leftText">
           <AnimateOnScroll reappear>
             <p>
             Startup homes offer a robust program that pairs each resident with a coach to provide support and foster a welcoming community within each space.            From undergraduate students to established industry professionals looking for a change of pace, startup houses are home to entrepreneurs of many backgrounds. 
@@ -128,6 +144,59 @@ export default function Scrolly(){
           <p>The SF Parc startup house helped Arraya and others along the way, offering networking opportunities and spaces for collaboration. “We host fireside chats and dinners where we invite tech luminaries to offer their insights. We try to prevent burnout by making everything flexible” said Grant.  
           </p>
         </PairStick>
+
+        <div className="rightContainer">
+          <div className="rightText">
+          <p>Even the most intimate spaces — kitchens, living rooms, bedrooms — hum with the sounds of laptop keyboards and business conversations. “When you live and work with a lot of doers, you get more stuff done,” said Grant.
+          </p><br />
+          <p>Let Arraya show you around the second floor. 
+          </p>
+          </div>
+        </div>
+
+
+        <PairStick imageSrc={bedroom}
+        imageAlt="bedroom illustration"
+        blocks={bedText}>
+          <p>The house is currently at capacity with 12 residents, but when a room opens up, Ryland Pozniak Daniels, 23, is looking to join The Residency. He launched ARLingo in December of 2024, an app that helps users learn new languages through virtual reality. 
+          </p><br />
+          <p>
+          “I’m interested in joining a San Francisco startup house for the fast-paced and indie-hacker environment,” Daniels said. He is looking for a potential partner who shares his passions for VR and building worlds to explore. “Most of my friends took corporate tech jobs. I want to do my own thing — my own startup. That’s my impetus for coming to Silicon Valley. That’s the way that someone like me can make a difference.”
+          </p><br />
+          <p>
+          Daniels shares sentiments with Nick Linck, founder of The Residency and its San Francisco location, SF Parc. “I have a ton of ideas I want to exist in the world,” Linck said. 
+          </p><br />
+        </PairStick>
+
+        <div className="rightContainer">
+        <div className="rightText">
+        <p>Linck used to work on five startups at once, but found it unsustainable. The Residency was one of his ideas that enabled people to realize more ideas in a social environment. 
+        </p><br />
+        <p>“The main reason people go to college is the social experience,” he said. “So let’s recreate that experience but let people do their own thing, their own startups or ideas.”
+        </p>
+
+        </div>
+        </div>
+
+        <div className="leftText">
+        <p>SF Parc has a waitlist of over 300 people, so it’s likely to be around for a while. Grant and Linck aim to expand the model while maintaining a sense of community. 
+        </p><br />
+        <p>“There’s no ideal persona for a startup house, just someone who is curious. We need to focus on community, continue to value authenticity, and encourage people to be themselves,” Grant said.
+        </p>
+        </div>
+
+        <div className="finalImage">
+          <Image
+          src={skyline}
+          alt="illustration of a skyline"
+          fill
+          style={{ objectFit: 'cover' }}/>
+        </div>
+
+        <div className="finalBlurb">
+          <p>Although the pandemic stalled the growth of the tech industry — and the Bay Area’s economy overall — the newer AI startup scene shows promise for development. AI startups have leased an additional 3.9 million square feet of downtown San Francisco office space since 2019, according to a 2024 study by CRE Daily.</p>
+          <br /><p>Could this expansion extend to startup houses? Grant hopes so. “Eventually I see this as a way to fund other hacker houses around the globe.”</p>
+        </div>
 
 
           
